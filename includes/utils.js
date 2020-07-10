92defb0dbb4c07f8712898d3f3b4d67d00214096
Saadat Qadri <saadat@appliedintelligence.co>
2020-07-09 23:02:24 -0300

    changes to bq hash function.

--- includes/utils.js
+++ includes/utils.js
@@ -1,9 +1,5 @@
-function hash(s) {
-    // simple hash function used to obfuscate strings
-    
-    for(var i = 0, h = 0xdeadbeef; i < s.length; i++)
-        h = Math.imul(h ^ s.charCodeAt(i), 2654435761);
-    return (h ^ h >>> 16) >>> 0;
+function hash(expression) {
+    return `farm_fingerprint(${expression})`;
 }
 
 module.exports = {
